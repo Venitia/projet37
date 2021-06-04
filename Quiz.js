aee20bba8248cafcd9d6e2ab136ec6e702739820
Venitia <76776212+Venitia@users.noreply.github.com>
2021-06-04 11:36:06 +0100

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,61 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+
+    //write code to change the background color here
+   background ("yellow");
+
+    
+    
+
+     Contestant.getPlayerInfo();
+
+    //write condition to check if contestantInfor is not undefined
+
+    if(allContestants !== undefined){
+    fill ("blue");
+    textSize(20);
+    text("*NOTE: Contestant who answered correct are highlightd in green !!",130,230);
+    }
+
+    //write code to highlight contest who answered correctly
+    for(var plr in allContestants){
+      var correctAns = "2";
+      if(correctAns === allContestants[plr].answer){
+        fill ("green");
+      }
+      else{
+        fill ("red");
+      }
+    }
+  }
+
+}
